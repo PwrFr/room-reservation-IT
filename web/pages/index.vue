1f467129f6ab4aa75f5348323aad0e89ed66bc6a
PwrFr <54875724+PwrFr@users.noreply.github.com>
2022-05-22 18:18:45 +0700

    use query

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -2,26 +2,40 @@
   <v-app>
     <v-container grid-list-xs>
       <h1 class="text-3xl font-bold text-right">หน้าหลัก</h1>
+      {{$apolloData.loading?"Loading":$apolloData.data}}
 
-      {{user}}
+
+      <!-- {{item}} -->
     </v-container>
   </v-app>
 </template>
 
 <script>
-import gql from 'graphql-tag'
+// import user from '~/apollo/queries/user.gql'
+import gql from 'graphql-tag';
+
+const ALL_USER = gql`
+query user{
+	user{
+    id
+    name
+  }
+
+}`
 export default {
   name: "IndexPage",
+   apollo: {
+    user: {
+      query: ALL_USER,
+      prefetch: true,
+    },
+  },
   data: () => ({
-    user: []
+    // user: []
   }),
 
-  apollo: {
-    user: {
-      prefetch: true,
-      query: ``
-    }
-  }
+
+ 
   
 };
 
