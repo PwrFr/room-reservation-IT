d62617e513b098c3f7db0d4a5bb5aaee29d8da76
Nutchayaporn <54405491+Nutchayaporn@users.noreply.github.com>
2022-05-23 17:24:45 +0700

    Update index.vue

--- web/pages/booking/index.vue
+++ web/pages/booking/index.vue
@@ -2,138 +2,120 @@
     <v-app>
         <v-container grid-list-xs>
             <h1 class="text-3xl font-bold text-right mb-8">หน้าการจอง</h1>
-            <v-card elevation="5" outlined>
-                <v-row no-gutters align="center" justify="center">
-                    <v-col cols="12" sm="6" md="4" class="py-6 font-bold text-3xl text-center">
-                        <span>
-                            M 22
-                        </span>
-                    </v-col>
+        
+            <v-col cols="12" class="py-2">
+                
+                <v-btn-toggle v-model="text" color="deep-purple accent-3" group>
+                    <v-btn value="all">
+                        <span class="hidden-sm-and-down">All</span>
+                    </v-btn>
 
-                    <v-col cols="12" sm="6" md="3" class="py-6">
-                        <span class="font-bold">
-                            Reserve Date
-                        </span><br>
-                        23 / 5 /2022 10 : 52 :12
-                    </v-col>
+                    <v-btn value="pending">
+                        <span class="hidden-sm-and-down">Pending</span>
+                    </v-btn>
 
-                    <v-col cols="12" sm="6" md="3" class="py-6">
-                        <span class="font-bold">
-                            Attendee
-                        </span><br>
-                        60
-                    </v-col>
+                    <v-btn value="approved">
+                        <span class="hidden-sm-and-down">Approved</span>
+                    </v-btn>
 
-                    <v-col cols="12" sm="6" md="2" class="py-6">
-                        <span class="font-bold">
-                            Status
-                        </span><br>
-                        Pending...
-                    </v-col>
-                </v-row>
-            </v-card>
+                    <v-btn value="rejected">
+                        <span class="hidden-sm-and-down">Rejected</span>
+                    </v-btn> 
+                    {{text}}
+                </v-btn-toggle>
+            </v-col>
 
+            <template v-for="(item, i) in items">
+                <v-col :key="i" cols="12" md="12">
+                    <v-hover v-slot="{ hover }">
+                        <v-card :elevation="hover ? 12 : 2" :class="{ 'on-hover': hover }">
+                            <v-row no-gutters align="center">
+                                <v-col cols="12" sm="4" md="4" class="py-6 font-bold text-3xl text-center">
+                                    <span>
+                                        {{ item.room }}
+                                    </span>
+                                </v-col>
 
+                                <v-col cols="12" sm="3" md="3" class="py-6 pl-3">
+                                    <span class="font-bold">
+                                        Reserve Date
+                                    </span><br>
+                                    {{ item.reserveDate }}
+                                </v-col>
 
+                                <v-col cols="12" sm="3" md="3" class="py-6 pl-3">
+                                    <span class="font-bold">
+                                        Attendee
+                                    </span><br>
+                                    {{ item.attendee }}
+                                </v-col>
 
- <template v-for="(item, i) in items">
-        <v-col
-          :key="i"
-          cols="12"
-          md="4"
-        >
-          <v-hover v-slot="{ hover }">
-            <v-card
-              :elevation="hover ? 12 : 2"
-              :class="{ 'on-hover': hover }"
-              
-            >
-              <v-img
-                :src="item.img"
-                height="225px"
-              >
-                <v-card-title class="text-h6 white--text">
-                  <v-row
-                    class="fill-height flex-column"
-                    justify="space-between"
-                  >
-                    <p class="mt-4 subheading text-left">
-                      {{ item.title }}
-                    </p>
-
-                    <div>
-                      <p class="ma-0 text-body-1 font-weight-bold font-italic text-left">
-                        {{ item.text }}
-                      </p>
-                      <p class="text-caption font-weight-medium font-italic text-left">
-                        {{ item.subtext }}
-                      </p>
-                    </div>
-                  </v-row>
-                </v-card-title>
-              </v-img>
-            </v-card>
-          </v-hover>
-        </v-col>
-      </template>
-
-
-
+                                <v-col cols="12" sm="2" md="2" class="py-6 pl-3">
+                                    <span class="font-bold">
+                                        Status
+                                    </span><br>
+                                    {{ item.status }}
+                                </v-col>
+                            </v-row>
+                        </v-card>
+                    </v-hover>
+                </v-col>
+            </template>
         </v-container>
     </v-app>
-
-
-    
 </template>
 
 
 
 <script>
 export default {
- data: () => ({
-      items: [
-        { 
-          room : "M22",
-          reserveDate: "23/5/2022 10:15:20",
-          attendee: "60",
-          status: "Pending..."   , 
-          title: 'New Releases',
-          text: `It's New Release Friday`,
-          subtext: 'Newly released songs. Updated daily.',
-          img: 'https://images.unsplash.com/photo-1429514513361-8fa32282fd5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3264&q=80',
-        },
-        {
-          title: 'Rock',
-          text: 'Greatest Rock Hits',
-          subtext: 'Lose yourself in rock tunes.',
-          img: 'https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
-        },
-        {
-          title: 'Mellow Moods',
-          text: 'Ambient Bass',
-          subtext: 'Chill beats to mellow you out.',
-          img: 'https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80',
-        },
-      ],
-      transparent: 'rgba(255, 255, 255, 0)',
-    }),
+    data: () => ({
+         text: 'all',
+        items: [
+            {
+                room: "M22",
+                reserveDate: "22/5/2022 10:15:20",
+                attendee: "60",
+                status: "Pending..."
 
-    
+            },
+            {
+                room: "M23",
+                reserveDate: "23/5/2022 10:15:20",
+                attendee: "61",
+                status: "Pending..."
+
+            },
+            {
+                room: "M24",
+                reserveDate: "24/5/2022 10:15:20",
+                attendee: "62",
+                status: "Pending..."
+
+            },
+            {
+                room: "M25",
+                reserveDate: "25/5/2022 10:15:20",
+                attendee: "63",
+                status: "Approved..."
+
+            }
+        ],
+    }),
 };
 </script>
 
 
 <style scoped>
 .v-card {
-  transition: opacity .4s ease-in-out;
+    transition: opacity .2s ease-in-out;
 }
 
 .v-card:not(.on-hover) {
-  opacity: 0.6;
- }
+    opacity: 0.7;
+}
 
 .v-application--wrap {
     min-height: 94vh
 }
-
 </style>
