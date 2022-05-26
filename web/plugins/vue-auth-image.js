c61479fadaf076f5203dce2f2cbfc968b4fede67
Nutchayaporn <54405491+Nutchayaporn@users.noreply.github.com>
2022-05-26 17:40:22 +0700

    test

--- web/plugins/vue-auth-image.js
+++
@@ -1,7 +0,0 @@
-import Vue from "vue";
-import VueAuthImage from "vue-auth-image";
-
-Vue.use(VueAuthImage);
-
-// var authHeader = "Bearer " + localStorage.getItem("auth._token.google");
-// Vue.prototype.$axios.defaults.headers.common["Authorization"] = authHeader;
