1bb9667b0a444cf6acd5c9f28b179f2823251830
kaolnwza <kaow_jijuy999@hotmail.com>
2022-05-27 12:17:53 +0700

    update docker and fix request query

--- api/server.go
+++ api/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/PwrFr/gqlgen/config"
@@ -18,7 +19,12 @@ func main() {
 	router.Handle("/", playground.Handler("GraphQL Playground", "/query"))
 	router.Handle("/query", config.ServerConfig())
 
-	err := http.ListenAndServe(":3001", router)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	err := http.ListenAndServe(":"+port, router)
 	if err != nil {
 		panic(err)
 	}
